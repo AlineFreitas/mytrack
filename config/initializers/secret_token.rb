b069b40c114d73a84b9782ca8d05187e357a2210
Aline de Oliveira Freitas <dreamofaline@gmail.com>
2011-11-12 15:08:02 -0200

    Just created the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytrack::Application.config.secret_token = '33d0ba2aed89136b76b146b84ac2d65f0d13c704b5649aefda8b74a74d112e21f16ecdd962d950fd48619ba1f225026ced18062ef8eaa1456f74b9fe265fad98'
